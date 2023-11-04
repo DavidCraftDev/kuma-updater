3fcbb46481a866c2d4e993fd248d8bc68085926e
David <david@davidcraft.de>
2023-11-04 19:05:08 +0100

    Update healthcheck.sh
    Signed-off-by: David <david@davidcraft.de>

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,7 +4,7 @@ if ! [ -f /tmp/kuma.json ]; then
   exit 1
 fi
 
-if [ -f /tmp/kuma.json ] && [ "$(jq -r .status </tmp/kuma.json)" != "success" ]; then 
+if [ -f /tmp/kuma.json ] && [ "$(jq -r .status </tmp/kuma.json)" != "{"ok":true}" ]; then 
   exit 1
 fi
 
